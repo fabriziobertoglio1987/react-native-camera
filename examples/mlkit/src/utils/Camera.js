831d5bd51e750c6c1a08297a2402757c7fefe3cb
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-03-31 16:48:02 +0200

    'feat(android): mlkit example for refactoring bran'

---
+++ examples/mlkit/src/utils/Camera.js
@@ -0,0 +1,28 @@
+import React, { useCallback } from 'react';
+import { RNCamera } from 'react-native-camera';
+import { useDimensions } from '@react-native-community/hooks';
+import { PERMISSIONS } from './constants';
+import { getFramingRect } from './Rectangle';
+
+const Camera = () => {
+  const dimensions = useDimensions();
+  const onBarCodeRead = useCallback((result) => {
+    if (result) {
+      const { type, data } = result;
+      if (data) {
+        console.log('code', data);
+      }
+    }
+  }, []);
+  return (
+    <RNCamera
+      captureAudio={false}
+      onBarCodeRead={onBarCodeRead}
+      rectOfInterest={getFramingRect(dimensions.screen)}
+      androidCameraPermissionOptions={PERMISSIONS}
+      style={{ height: dimensions.screen.height }}
+      type={RNCamera.Constants.Type.back} />
+  );
+}
+
+export default Camera;
