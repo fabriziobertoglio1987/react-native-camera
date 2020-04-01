dab04fc61f02466fcf56310b0404346573a5dbb7
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-04-01 11:44:09 +0200

    fixing bug

---
+++ examples/mlkit/src/utils/rectangle.js
@@ -0,0 +1,21 @@
+export function getFramingRect(screen) {
+  const HEIGHT = 240;
+  const WIDTH = 240;
+  const leftOffset = (screen.width - WIDTH) / 2;
+  const topOffset = (screen.height - HEIGHT) / 2;
+
+  // return {
+  //   x: leftOffset,
+  //   y: topOffset,
+  //   width: leftOffset + WIDTH,
+  //   height: topOffset + HEIGHT,
+  //   landscapeMode: ?,
+  // };
+  return {
+    x: 500,
+    y: 192,
+    width: 500,
+    height: 400,
+    landscapeMode: false,
+  };
+}
